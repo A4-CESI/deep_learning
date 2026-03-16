ee391af0a64175e56d1219138c6f78d39dc97bc3
Gandouz Akrem <gandouzakrem48@gmail.com>
2026-03-16 10:28:49 +0100

    read data

---
+++ read_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv("C:\\Users\\LENOVO\\Desktop\\IA-Deep Learning\\Data\\diabetes_binary_health_indicators_BRFSS2015.csv")
+
+print(df.head()) ,
+print(df.info())
